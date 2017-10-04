b24c3a9d13de0895c4c6456909667d6da3c0017e
Dallin Smith <dallinbsmith@gmail.com>
2017-10-03 19:47:28 -0600

    axios and pulling APIs

--- exercises/to_do_list/main.js
+++ exercises/to_do_list/main.js
@@ -1,46 +1,26 @@
 function axiosStuff() {
-axios.get("https://api.vschool.io/ericnatejones/todo", newTodo).then(function (response) {
-        conosole.log
-    });
-}
+    axios.get("https://api.vschool.io/dallin/todo").then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                document.getElementById("todoh1").innerHTML += "<h2>" + response.data[i].title + "</h2>"
+            };
+        }
+
+    )
+};
+
 
 axiosStuff();
 
 
-//var xhr = new XMLHttpRequest();
-//
-//xhr.onreadystatechange = function() {
-//    if (xhr.readyState === 4 && xhr.status === 200) {
-//        console.log(JSON.parse(xhr.responseText));
-//    }
-//}
-//
-//xhr.open("GET", "https://swapi.co/api/starships/9");
-//xhr.send();
+function newToDo() {
+    let inputboxvar = (document.getElementById("newtodobox").value);
+    let inputboxvar1 = (document.getElementById("newtododescription").value);
+    const newThing = {
+        title: inputboxvar,
+        description: inputboxvar1
+    };
+    axios.post("https://api.vschool.io/dallin/todo", newThing).then(function (response) {
+        console.log(response);
 
-//axios.get("https://api.vschool.io/ericnatejones/todo").then(function(response) {
-//    // Do your coding here
-//});
-
-
-//var newTodo = {
-//    title: "Buy fruit",
-//    description: "Not carrits, although they're technically a tuber"
-//}
-
-//axios.post("https://api.vschool.io/ericnatejones/todo", newTodo).then(function(response) {
-//    console.log(response.data);
-//});
-
-
-//axios.delete("https://api.vschool.io/ericnatejones/todo/59cd38608e509d2c3e49e030").then(function(response) {
-//    console.log(response.data);
-//});
-
-//var update = {
-//    completed: false
-//}
-//
-//axios.put("https://api.vschool.io/ericnatejones/todo/58fa565b2c425d27974faf7e", update).then(function(response) {
-//    console.log(response.data);
-//});
+    })
+};
