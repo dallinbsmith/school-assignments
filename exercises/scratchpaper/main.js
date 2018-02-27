d6ad6dfbb51f8ff6e9bf487908c1364ef30b75a0
Dallin Smith <dallinbsmith@gmail.com>
2018-02-27 07:31:24 -0700

    made a bunch of updates

--- exercises/scratchpaper/main.js
+++ exercises/scratchpaper/main.js
@@ -1,47 +1,36 @@
-//
-//
-// function makeArrayConsecutive2(statues) {
-//     let newnewArray = [];
-//     let newArray = numArray.sort(function (a, b) {  return a - b;  });
-//     // for (let i = 0; i < newArray.length; i++){
-//     //     if (newArray[i + 1] !== newArray[i] + 1){
-//             console.log(newArray)
-//     //         newnewArray.push(newArray[i] + 1)
-//     }
-//
-//
-// makeArrayConsecutive2(6, 3)
+function avoidObstacles(inputArray) {
+    let sorted = inputArray.sort(function(a, b) {return a - b;});
+    const missing = [];
+    let answer = 0
+    let count = 0
+    let runAgain = true
+    for(var i=1;i<sorted[sorted.length -1];i++) {
+        if(sorted.indexOf(i) == -1){
+        missing.push(i);
+        }
+    }
+    function ridTheMissing(inputArr, num){
+         for(var i=1;i<sorted[sorted.length -1];i++) {
+            if (inputArr[i] % missing[count] === 0){
+                count++
+                break
+            }else if(inputArr[i] === sorted[sorted.length -1] && inputArr[i] % missing[count] !== 0) {
+                runAgain = false     
+            }
+         }
+            if (runAgain === true){
+                ridTheMissing(inputArr, count) 
+            } else{
+            answer = num; 
+         }
+    }
+    ridTheMissing(inputArray, count);
+    if (missing[answer] === undefined){
+        return inputArray[inputArray.length - 1] + 1
+    }else{
+    return missing[answer]
+    }
+}
 
-
-// function one(){
-//     setTimeout(function(){
-//       console.log("1. First thing settung up second thing");
-//       setTimeout(function(){
-//         console.log("2. First thing settung up second thing");
-//         setTimeout(function(){
-//           console.log("3. First thing settung up second thing");
-//           setTimeout(function(){
-//             console.log("4. First thing settung up second thing");
-//           }, 2000);
-//         }, 2000);
-//       }, 2000);
-//     }, 2000);
-//   };
-
-
-//BETTER WAY TO DO STUFF ^^^^
-
-// function promise(){
-//   return new Promis((resolve, reject) => {
-//     console.log("1. First thing settung up second thing");
-//           setTimeout(function(){
-//             resolve("resolve with some value");
-//     }, 2000)
-//   })
-// }
-//
-// promise()
-// .then(res =>console.log(res))
-//
-// jdaryl - Git star-wars
-// promises in action.
+    
+console.log(avoidObstacles([2, 3])) 
